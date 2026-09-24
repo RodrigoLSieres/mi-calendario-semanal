46c9da3984bdd1130c1c7b067c111858e9ca4ea2
RodrigoLSieres <rl_sieres@hotmail.es>
2026-09-24 21:08:49 +0200

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,17 @@
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const cacheNames = await caches.keys();
+    await Promise.all(cacheNames.map(name => caches.delete(name)));
+    await self.registration.unregister();
+    const clientsList = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
+    for (const client of clientsList) {
+      client.navigate(client.url);
+    }
+  })());
+});
+
+self.addEventListener('fetch', () => {
+  // No se interceptan peticiones. Este archivo solo elimina la PWA antigua.
+});
